25574e9608429218b0dc424bdb0386de10024ebf
optimisticlucifer <b20314@students.iitmandi.ac.in>
2023-10-25 23:08:20 +0530

    react-router-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home';
+import Resume from './components/resume'
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hi i am portfolio</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
+
   );
 }
 
---
+++ src/components/sec4/index.js
@@ -0,0 +1,9 @@
+function Sec4() {
+    return (
+        <div>
+            <h4>sec4</h4>
+        </div>
+    );
+}
+
+export default Sec4;
