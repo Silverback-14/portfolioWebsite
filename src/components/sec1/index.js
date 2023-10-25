a617c0ccd68240b087b9496d3e4d1511b7981966
optimisticlucifer <b20314@students.iitmandi.ac.in>
2023-10-25 23:54:42 +0530

    model added

--- src/components/sec1/index.js
+++ src/components/sec1/index.js
@@ -1,7 +1,19 @@
+import MyModel from './model.js';
+
 function Sec1() {
+    const divStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+    };
+
     return (
-        <div>
-            <h1>sec1</h1>
+        <div style={divStyle}>
+            <div>
+                <h1>sec1</h1>
+            </div>
+            <div>
+                <MyModel />
+            </div>
         </div>
     );
 }
---
+++ src/components/sec1/model.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function MyModel() {
+    return (
+        <Spline scene="https://prod.spline.design/XWDbUykYBoAARJ8I/scene.splinecode" />
+    );
+}
